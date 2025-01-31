0029d4d950abd823366d27e9cf2b437cdc07aa7e
Duke Tuxem <33418846+duketuxem@users.noreply.github.com>
2025-01-31 10:37:11 +0100

    tmux as default yes + dwm cosmetic bar

--- config.h
+++ config.h
@@ -11,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "CodeNewRoman Nerd Font Mono:size=14", "Noto Sans Mono CJK TC:size=12" };
-static const char dmenufont[]       = "CodeNewRoman Nerd Font Mono:size=14";
+static const char *fonts[]          = { "CodeNewRoman Nerd Font Mono:size=10", "Noto Sans Mono CJK TC:size=8" };
+static const char dmenufont[]       = "CodeNewRoman Nerd Font Mono:size=10";
 static const char col_gray1[]       = "#222222"; // Background, also for dmenu
 static const char col_gray2[]       = "#27504f"; // Blue Marrs Green
 static const char col_gray3[]       = "#e3a939"; // Yellow Marrs Green
@@ -73,7 +73,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray2, "-sf", col_gray3, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "st", "-e", "/usr/bin/tmux", NULL };
 
 /* shortcuts */
 static const char *browser[]  = { "firefox", NULL };
@@ -84,7 +84,6 @@ static const char *browser[]  = { "firefox", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_s,      spawn,          {.v = dmenucmd } },
-	/* { MODKEY|ShiftMask,             XK_Return, spawn,          SHCMD("st terminal") }, */
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
